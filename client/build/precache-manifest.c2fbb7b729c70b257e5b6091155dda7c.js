5f21772df9bf7a4a0917a18073bc33b2b5b0ebb6
Austin Beaufort <tinyhatcoding@gmail.com>
2019-05-08 17:26:12 -0400

    test

---
+++ client/build/precache-manifest.c2fbb7b729c70b257e5b6091155dda7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f88cc18e9af3ea4326004afb376cd8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "714d26feb09bed0100c6",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "407b0af4e7311bac4612",
+    "url": "/static/css/main.251f4508.chunk.css"
+  },
+  {
+    "revision": "714d26feb09bed0100c6",
+    "url": "/static/js/2.c9a76ab0.chunk.js"
+  },
+  {
+    "revision": "407b0af4e7311bac4612",
+    "url": "/static/js/main.9252a570.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://austinb:austin123@cluster0-dzqj8.mongodb.net/test?retryWrites=true'
+}
